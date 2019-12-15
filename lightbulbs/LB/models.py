863241a07b9fa6b15a4f7ed0287f2d5726714622
Hanif Ali <alihanif016@gmail.com>
2019-12-15 15:35:06 +0500

    Added four models. 'Lightbulb' for individual threads 'Proposal' for requests to participate in the threads. 'Message' for inter-user messages 'Notification' for site-user messaging.

--- lightbulbs/LB/models.py
+++ lightbulbs/LB/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class LBUser(AbstractUser):
+    """LightBulb User Model. Extends the default django User Model to act as an
+    Authentication Model. Additional Fields are defined"""
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     description = models.CharField(max_length=30, blank=True)
@@ -19,7 +22,57 @@ class LBUser(AbstractUser):
     
     
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name}"
 
     class Meta:
         verbose_name = "LBUser"
+
+
+class Lightbulb(models.Model):
+    """Model for Individual Ideas"""
+
+    creator = models.ForeignKey(LBUser, on_delete=models.CASCADE, related_name='lightbulbs')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.creator} - {self.title}"
+
+
+class Proposal(models.Model):
+    """Model for Proposals sent by users for participating in an idea"""
+    
+    sender = models.ForeignKey(LBUser, on_delete=models.CASCADE, related_name="sent_proposals")
+    lightbulb = models.ForeignKey(Lightbulb, on_delete=models.CASCADE, related_name="proposals")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"Proposal from {self.name} for {self.lightbulb.title}"
+
+
+class Message(models.Model):
+    """Model for messages"""
+    
+    sender = models.ForeignKey(LBUser, on_delete=models.CASCADE, related_name="sent_messages")
+    receiver = models.ForeignKey(LBUser, on_delete=models.CASCADE, related_name="received_messages")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.sender} - {self.message}"
+
+
+class Notifiation(models.Model):
+    """Model for notifications. Includes a link which is the page that the app will go to 
+    upon clicking that link"""
+
+    user = models.ForeignKey(LBUser, on_delete=models.CASCADE, related_name="notifications")
+    message = models.CharField(max_length=50)
+    link = models.URLField()
+
+    def __str__(self):
+        return f"{self.message}"
