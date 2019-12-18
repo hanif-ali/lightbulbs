0859160f8c0ffd737ebd5b0286a5ea698cd93e86
Hanif Ali <alihanif016@gmail.com>
2019-12-19 01:33:12 +0500

    Fixed async upvote, downvote and star buttons

--- lightbulbs/LB/static/js/user.js
+++ lightbulbs/LB/static/js/user.js
@@ -5,36 +5,62 @@ star_buttons.forEach(element => {
 
         var idea_id = element.dataset.idea;
         var url = `http://${window.location.hostname}:${window.location.port}/star/${idea_id}`  
-        fetch(url).then((response)=>{
-            var response_data = response.json()
-            console.log(response_data);
-            if(response_data["success"] == "starred" || response_data["status"] == "unstarred"){
-                element.classList.toggle("fa");
-                element.classList.toggle("fas");
-                element.classList.toggle("text-warning");
+        fetch(url).then(res => res.json())
+                .then(data => {
+                    if(data["status"] == "starred"){
+                        element.classList.add("fa");
+                        element.classList.add("fas");
+                        element.classList.add("text-warning");
+                    }
+                    else if (data["status"] == "unstarred"){
+                        element.classList.remove("fa");
+                        element.classList.remove("fas");
+                        element.classList.remove("text-warning");
 
-            }
-        });
-    });
+                    }
+                })
+            })
 });
 
 var upvote_buttons = document.querySelectorAll(".upvote-button")
 
 upvote_buttons.forEach(element => {
     element.addEventListener("click", function(){
-        // Do some upvote logic
-        element.classList.toggle("text-warning")
-    })
+        var idea_id = element.dataset.idea;
+        var url = `http://${window.location.hostname}:${window.location.port}/upvote/${idea_id}`  
+        fetch(url).then(res => res.json())
+                .then(data => {
+                    if(data["status"] == "upvoted"){
+                        element.classList.add("text-warning")
+                    }
+
+                    else if (data["status"] == "unupvoted"){
+                        element.classList.remove("text-warning")
+
+                    }
+                })
+            })
 })
 
 var downvote_buttons = document.querySelectorAll(".downvote-button")
 
 downvote_buttons.forEach(element => {
     element.addEventListener("click", function(){
-        // Do some upvote logic
-        element.classList.toggle("text-warning")
+        var idea_id = element.dataset.idea;
+        var url = `http://${window.location.hostname}:${window.location.port}/downvote/${idea_id}`  
+        fetch(url).then(res => res.json())
+                .then(data => {
+                    if(data["status"] == "downvoted"){
+                        element.classList.add("text-warning")
+                    }
+
+                    else if (data["status"] == "undownvoted"){
+                        element.classList.remove("text-warning")
+
+                    }
+                })
+            })
     })
-})
 
 
 
