d852a28b643ba648c9e858fddcf7c8d246d06eec
Hanif Ali <alihanif016@gmail.com>
2019-12-20 02:51:06 +0500

    Added some basic tests Added dummy data

--- lightbulbs/LB/tests.py
+++ lightbulbs/LB/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
+from LB.models import LBUser
 
-# Create your tests here.
+class TestOuterViews(TestCase):
+    def test_home(self):
+        res = self.client.get(reverse("home-page"))
+        self.assertEquals(res.status_code, 200)
+
+    def test_login(self):
+        res = self.client.get(reverse("login"))
+        self.assertEquals(res.status_code, 200)
+
+    def test_registration(self):
+        res = self.client.get(reverse("register"))
+        self.assertEquals(res.status_code, 200)
+
+
+class TestMiddleViews(TestCase):
+    def setUp(self):
+        user1 = LBUser(first_name="Test", last_name="User", email="test@test.com", username="test", age=19)
+        user1.set_password("test")
+        user1.save()
+
+    def test_login_fail(self):
+        res = self.client.post(reverse("login"), {
+            "username": "test",
+            "password": "wrong" 
+        })
+        self.assertEquals(res.url, reverse("login"))
+
+
+    def test_login_pass(self):
+        res = self.client.post(reverse("login"), {
+            "username": "test",
+            "password": "test" 
+        })
+        self.assertEquals(res.status_code, 200)
+        self.assertEquals(res.url, reverse("feed"))
+
+    def test_logout(self):
+        res = self.client.post(reverse("logout"))
+        self.assertEquals(res.status_code, 200)
+        self.assertEquals(res.url, reverse("login"))
+
+
